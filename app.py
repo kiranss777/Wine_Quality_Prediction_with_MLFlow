0a698b052f00d5bdb925a16804c5b39726406186
Kiran-Sathya-SR <sathyasunkojirao.k@northeastern.edu>
2024-10-15 17:18:42 -0400

    Predict functionality and Dockerfile code added

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-from flask import Flask,render_template,request
-import os
+from flask import Flask, render_template, request
+import os 
 import numpy as np
 import pandas as pd
 from mlProject.pipeline.prediction import PredictionPipeline
 
-app=Flask(__name__)
 
-@app.route('/',methods=['GET'])
+app = Flask(__name__) # initializing a flask app
+
+@app.route('/',methods=['GET'])  # route to display the home page
 def homePage():
     return render_template("index.html")
 
-@app.route('/train',methods=['GET'])
+
+@app.route('/train',methods=['GET'])  # route to train the pipeline
 def training():
     os.system("python main.py")
-    return "Training Done . "
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8080,debug=True)
+    return "Training Successful!" 
 
 
 @app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
